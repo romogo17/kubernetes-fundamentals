9113e2e292a2f047ade176e92c41799a6c562ef9
Roberto Mora <romogo17@gmail.com>
2021-01-10 21:42:23 -0600

    Added infra for 3.2: Grow the Cluster

---
+++ infra/3_2-grow-the-cluster/main.tf
@@ -0,0 +1,90 @@
+variable "gcp_project" {
+  type        = string
+  description = "The id of Google Cloud project in which the infrastructure will be deployed"
+}
+
+provider "google" {
+  project = var.gcp_project
+  region  = "us-east1"
+  zone    = "us-east1-b"
+}
+
+resource "google_compute_instance" "vm_instance_master" {
+  count        = 1
+  name         = "kube-master-node${count.index + 1}"
+  machine_type = "e2-standard-2"
+
+  boot_disk {
+    initialize_params {
+      image = "centos-cloud/centos-8"
+    }
+  }
+
+  network_interface {
+    network    = google_compute_network.vpc_network.self_link
+    subnetwork = google_compute_subnetwork.vpc_subnetwork.self_link
+    access_config {
+      // Ephemeral IP
+    }
+  }
+
+  metadata = {
+    ssh-keys = "kube-user:${file("~/.ssh/id_rsa.pub")}"
+  }
+
+  labels = {
+    lab  = "3_1"
+    role = "master"
+  }
+}
+
+resource "google_compute_instance" "vm_instance_worker" {
+  count        = 1
+  name         = "kube-worker-node${count.index + 1}"
+  machine_type = "e2-standard-2"
+
+  boot_disk {
+    initialize_params {
+      image = "centos-cloud/centos-8"
+    }
+  }
+
+  network_interface {
+    network    = google_compute_network.vpc_network.self_link
+    subnetwork = google_compute_subnetwork.vpc_subnetwork.self_link
+    access_config {
+      // Ephemeral IP
+    }
+  }
+
+  metadata = {
+    ssh-keys = "kube-user:${file("~/.ssh/id_rsa.pub")}"
+  }
+
+  labels = {
+    lab  = "3_1"
+    role = "worker"
+  }
+}
+
+resource "google_compute_subnetwork" "vpc_subnetwork" {
+  name          = "kube-subnetwork-us-east1"
+  ip_cidr_range = "10.2.0.0/16"
+  region        = "us-east1"
+  network       = google_compute_network.vpc_network.self_link
+}
+
+resource "google_compute_firewall" "vpc_firewall" {
+  name    = "allow-all"
+  network = google_compute_network.vpc_network.self_link
+
+  allow {
+    protocol = "all"
+  }
+
+}
+
+resource "google_compute_network" "vpc_network" {
+  name                    = "kube-vpc"
+  auto_create_subnetworks = false
+}
